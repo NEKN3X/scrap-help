1304d5e47be6983e24e6fe899558cb2dd650f6f4
NEKN3X <43030839+NEKN3X@users.noreply.github.com>
2025-07-08 22:06:48 +0900

    型定義 fix

--- packages/core/src/help.ts
+++ packages/core/src/help.ts
@@ -1,8 +1,4 @@
-import type { Helpfeel } from './scrapbox.js'
-
-export type HelpContent = string
-
 export interface Help {
-  helpfeel: Helpfeel
-  content: HelpContent
+  helpfeel: string
+  content: string
 }
--- packages/core/src/scrapbox.ts
+++ packages/core/src/scrapbox.ts
@@ -1,33 +1,20 @@
-export type Helpfeel = string
-
-export type ProjectId = string
-export type ProjectName = string
-
 export interface Project {
-  id: ProjectId
-  name: ProjectName
+  id: string
+  name: string
   pages: Page[]
 }
 
-export type PageId = string
-export type PageTitle = string
-export type PageImage = string
-export type PageCreated = number
-export type PageUpdated = number
-
-export type LineId = string
-export type LineText = string
 export interface PageLine {
-  id: LineId
-  text: LineText
+  id: string
+  text: string
 }
 
 export interface Page {
-  id: PageId
-  title: PageTitle
-  image?: PageImage
-  created: PageCreated
-  updated: PageUpdated
-  helpfeels: Helpfeel[]
+  id: string
+  title: string
+  image?: string
+  created: number
+  updated: number
+  helpfeels: string[]
   lines: PageLine[]
 }
